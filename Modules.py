f454aa5f7f0bebc3e39a8afbad45f24c6c671c7a
Joel A <joel022003@gmail.com>
2024-11-07 23:23:41 +0530

    Update Modules.py

--- Modules.py
+++ Modules.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 def save_df(df, filename, ext) :
@@ -10,6 +11,11 @@ def save_df(df, filename, ext) :
     # Creates the directory and any necessary subdirectories
     dir_path.mkdir(parents=True, exist_ok=True)
 
-    df.to_csv(file_path, index=False)
+    if os.path.isfile(file_path) :
+        # Append an existing file without Header
+        df.to_csv(file_path, mode='a', index=False, header=False)
+    else :
+        # Create a new file with Header
+        df.to_csv(file_path, mode='w', index=False, header=True)
 
     print(f"File saved to {file_path}")
